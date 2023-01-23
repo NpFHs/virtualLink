e8736a0ebd8b8b262b8f493ad978bfd7cba204f9
NpFHs <noam.israel.sela@gmail.com>
2023-01-23 16:49:19 +0200

    bug fixes

--- encryption.py
+++ encryption.py
@@ -1,3 +1,10 @@
 import rsa
+
 public_key, private_key = rsa.newkeys(512)
 
+
+def encrypt(message):
+    enc = rsa.encrypt(message.encode(), public_key)
+
+    new_enc = enc.replace(b"\n", b"").replace(b"\r", b"")
+    return enc

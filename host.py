70e77d4217edb112e2a891c722f891b1bd860de5
noam <noam.israel.sela@gmail.com>
2023-05-24 15:58:59 +0300

    bug fixes

--- host.py
+++ host.py
@@ -14,30 +14,123 @@ IP: str = "0.0.0.0"
 PORT: int = 8091
 BREAK1 = "<BREAK1>"  # use to split between LEVEL1 data
 BREAK2 = "<BREAK2>"  # use to split between LEVEL2 data
+CLIENT_SCREEN_WIDTH = 750
 pre_commands = []  # Storing the last commands the user enter.
 current_command = 0  # Use to save the command location when the user press Up button.
 current_directory = "/"
-CLIENT_SCREEN_WIDTH = 750
 files_list = []
 public_key, private_key = rsa.newkeys(2048)
 client_public_key = None
-# indicate the current file location for tab_complete()
-current_file_in_files_list = 0
-# save the original command for tab_complete()
-pre_command = ""
-
+current_file_in_files_list = 0  # indicate the current file location for tab_complete()
+pre_command = ""  # save the original command for tab_complete()\
 is_files_list_change = False  # mark when get new files list
 is_screen_live = False
 
 
-# client_dist = "Unknown"
-
-
 # TODO: add multiple clients support. update: maybe not...
+
+
+class UserInterface(ttk.Frame):
+    def __init__(self, root):
+        ttk.Frame.__init__(self)
+
+        root.geometry("800x610")
+        tab_frame = ttk.Notebook()
+        tab_frame.pack(side="top", fill="both", expand=True, padx=5, pady=5)
+        client_dist = tk.StringVar(value="Unknown")
+        client_name = tk.StringVar(value="Unknown")
+        client_name_with_name = tk.StringVar(value="Unknown")
+        command = tk.StringVar(value="Enter a command")
+        files = tk.StringVar()
+        mono_font = font.Font(family="FreeMono")  # set the font to the msg_list
+
+        tabs = {}
+        for tab in ("Remote Desktop", "File Transfer", "Command Prompt", "Power Management"):
+            tabs[tab] = tk.Frame(tab_frame)
+            tab_frame.add(tabs[tab], text=tab)
+
+            # Add elements to the "Remote Desktop" tab
+            if tab == "Remote Desktop":
+                label = tk.Label(tabs[tab], text="Remote desktop view:")
+                label.pack(side="top", fill="x", padx=10, pady=10)
+
+                # create the started chart var
+                start_screen = ImageTk.PhotoImage(
+                    Image.open("./images/fullscreen.png").resize((CLIENT_SCREEN_WIDTH, CLIENT_SCREEN_WIDTH * 9 // 16)))
+
+                screen_label = ttk.Label(tabs[tab], image=start_screen)
+                # keep reference to the chart, so it doesn't get prematurely garbage collected at the end of the function
+                screen_label.image = start_screen
+                screen_label.pack()
+
+                get_screenshot_button = ttk.Button(tabs[tab], text="stop", command=lambda: stop_live_screen())
+                get_screenshot_button.pack(pady=20, padx=20, fill="y")
+
+                # canvas = tk.Canvas(tabs[tab], width=400, height=300, bd=1)
+                # canvas.pack(side="top", fill="both", expand=True, padx=10, pady=10)
+
+            # Add elements to the "File Transfer" tab
+            elif tab == "File Transfer":
+                label = ttk.Label(tabs[tab], text="Select a file to transfer:")
+                label.pack(side="top", fill="x", padx=10, pady=10)
+                file_button = ttk.Button(tabs[tab], text="Browse...", command=lambda: browse_files(root, client_socket))
+                file_button.pack(side="top", fill="x", padx=10, pady=10)
+
+                files_label = ttk.Label(tabs[tab], textvariable=files)
+                files_label.pack(side="bottom", padx=10, pady=10, anchor="e")
+
+            # TODO: export output to file option
+            # TODO: support "long live" commands
+            # TODO: return errors
+            # Add elements to the "Command Prompt" tab
+            elif tab == "Command Prompt":
+                messages_frame = ttk.Frame(tabs[tab])
+                scrollbar = ttk.Scrollbar(messages_frame)
+                msg_list = tk.Listbox(messages_frame, yscrollcommand=scrollbar.set, width=100, height=25,
+                                      selectbackground="#333333", highlightthickness=0, activestyle="none",
+                                      font=mono_font)
+                scrollbar["command"] = msg_list.yview
+                scrollbar.pack(side=tk.RIGHT, fill=tk.Y, pady=15)
+                msg_list.pack(side=tk.LEFT, fill=tk.BOTH, padx=10, pady=10)
+                messages_frame.pack()
+
+                # label = ttk.Label(tabs[tab], text="Enter a command:")
+                # label.pack(side="top", fill="x", padx=10, pady=10)  # NOQA
+                command_entry = ttk.Entry(tabs[tab], textvariable=command)
+                command_entry.bind("<Up>", lambda event: command_up(command))
+                command_entry.bind("<Down>", lambda event: command_down(command))
+                command_entry.bind("<Return>",
+                                   lambda event: send_button(client_socket, command_entry, msg_list, command))
+                command_entry.bind("<Tab>", lambda event: tab_complete(command_entry, command))
+                command_entry.bind("<Key>", lambda event: reset_pre_command())
+                command_entry.pack(side="top", fill="x", padx=10)
+                execute_button = ttk.Button(tabs[tab], text="Execute",
+                                            command=lambda: send_button(client_socket, command_entry, msg_list,
+                                                                        command))
+                execute_button.pack(side="top", fill="x", padx=10, pady=10)
+
+            # Add elements to the "Power Management" tab
+            elif tab == "Power Management":
+                shutdown_button = ttk.Button(tabs[tab], text="Shutdown", command=lambda: shutdown(client_socket))
+                shutdown_button.pack(side="top", fill="x", padx=10, pady=10)
+                restart_button = ttk.Button(tabs[tab], text="Restart", command=lambda: restart(client_socket))
+                restart_button.pack(side="top", fill="x", padx=10, pady=10)
+                logout_button = ttk.Button(tabs[tab], text="Log Out", command=lambda: log_out(client_socket))
+                logout_button.pack(side="top", fill="x", padx=10, pady=10)
+
+                ip_label = ttk.Label(tabs[tab], text=f"ip address: {client_address}")
+                ip_label.pack(side="bottom", padx=10, pady=5)
+                dist_label = ttk.Label(tabs[tab], textvariable=client_dist)
+                dist_label.pack(side="bottom", padx=10, pady=5)
+                name_label = ttk.Label(tabs[tab], textvariable=client_name_with_name)
+                name_label.pack(side="bottom", padx=10, pady=5)
+                panel_label = ttk.Label(tabs[tab], text="system information:")
+                panel_label.pack(side="bottom", padx=10, pady=10)
+
 
 def encrypt(msg):
     try:
-        enc_msg = rsa.encrypt(msg.encode(), client_public_key)
+        enc_msg = rsa.encrypt(msg.encode(), client_public_key)  # NOQA
     except:
         print("Encryption error")
         enc_msg = b"error"
@@ -82,11 +175,6 @@ def get_resp(client_socket):
         print(f"enc_resp: {enc_resp}")
 
         resp = enc_resp
-        # try:
-        #     resp = enc_resp.decode()
-        # except UnicodeDecodeError:
-        #     resp = "unknown message!"
-        #     print("unknown message!")
     else:
         print("No length info!")
 
@@ -343,6 +431,60 @@ def stop_live_screen():
     is_screen_live = False
 
 
+def command_up(command):
+    global current_command
+    if current_command > -len(pre_commands):
+        current_command -= 1
+        command.set(pre_commands[current_command])
+    else:
+        print("Can't go up anymore!")
+
+    reset_pre_command()
+
+
+def command_down(command):
+    global current_command
+    if -1 > current_command:
+        current_command += 1
+        command.set(pre_commands[current_command])
+    else:
+        print("Can't go down anymore!")
+
+    reset_pre_command()
+
+
+def tab_complete(entry, command):
+    global current_file_in_files_list, pre_command
+
+    # save the current command only at the first time tab pressed
+    if current_file_in_files_list == 0:
+        pre_command = command.get()
+
+    if current_file_in_files_list >= len(files_list):
+        current_file_in_files_list = 0
+
+    try:
+        file_name = files_list[current_file_in_files_list].split(BREAK2)[1]
+        command.set(f"{pre_command} {file_name}")
+        current_file_in_files_list += 1
+        entry.icursor(tk.END)
+    except IndexError:
+        pass
+    return "break"
+
+
+def send_button(client_socket, command_entry, msg_list, command):
+    global current_command, current_file_in_files_list
+    execute_command(client_socket, command_entry.get(), msg_list)
+    pre_commands.append(command.get())
+    command_entry.delete(0, tk.END)
+    current_command = 0
+
+    # include in bind <Key>.
+    # # reset the current file for tab_complete()
+    # current_file_in_files_list = 0
+
+
 def main():
     global is_screen_live
     root = tk.Tk()
@@ -384,8 +526,7 @@ def main():
 
                 elif msg_type == "public_key":
                     client_public_key = rsa.key.PublicKey.load_pkcs1(msg, format="DER")
-                    print(
-                        f"\ntype(client_public_key): {type(client_public_key)}\nclient_public_key: {client_public_key}\n")
+                    # print(f"\ntype(client_public_key): {type(client_public_key)}\nclient_public_key: {client_public_key}\n")  # NOQA
 
                 else:
                     print(f"Wrong message type! (message: {msg_type})")
@@ -393,56 +534,6 @@ def main():
                 break
             except OSError:
                 break
-
-    def command_up():
-        global current_command
-        if current_command > -len(pre_commands):
-            current_command -= 1
-            command.set(pre_commands[current_command])
-        else:
-            print("Can't go up anymore!")
-
-        reset_pre_command()
-
-    def command_down():
-        global current_command
-        if -1 > current_command:
-            current_command += 1
-            command.set(pre_commands[current_command])
-        else:
-            print("Can't go down anymore!")
-
-        reset_pre_command()
-
-    def tab_complete(entry):
-        global current_file_in_files_list, pre_command
-
-        # save the current command only at the first time tab pressed
-        if current_file_in_files_list == 0:
-            pre_command = command.get()
-
-        if current_file_in_files_list >= len(files_list):
-            current_file_in_files_list = 0
-
-        try:
-            file_name = files_list[current_file_in_files_list].split(BREAK2)[1]
-            command.set(f"{pre_command} {file_name}")
-            current_file_in_files_list += 1
-            entry.icursor(tk.END)
-        except IndexError:
-            pass
-        return "break"
-
-    def send_button():
-        global current_command, current_file_in_files_list
-        execute_command(client_socket, command_entry.get(), msg_list)
-        pre_commands.append(command.get())
-        command_entry.delete(0, tk.END)
-        current_command = 0
-
-        # include in bind <Key>.
-        # # reset the current file for tab_complete()
-        # current_file_in_files_list = 0
 
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server_socket.bind((IP, PORT))
@@ -505,13 +596,14 @@ def main():
             # label = ttk.Label(tabs[tab], text="Enter a command:")
             # label.pack(side="top", fill="x", padx=10, pady=10)  # NOQA
             command_entry = ttk.Entry(tabs[tab], textvariable=command)
-            command_entry.bind("<Up>", lambda event: command_up())
-            command_entry.bind("<Down>", lambda event: command_down())
-            command_entry.bind("<Return>", lambda event: send_button())
-            command_entry.bind("<Tab>", lambda event: tab_complete(command_entry))
+            command_entry.bind("<Up>", lambda event: command_up(command))
+            command_entry.bind("<Down>", lambda event: command_down(command))
+            command_entry.bind("<Return>", lambda event: send_button(client_socket, command_entry, msg_list, command))
+            command_entry.bind("<Tab>", lambda event: tab_complete(command_entry, command))
             command_entry.bind("<Key>", lambda event: reset_pre_command())
             command_entry.pack(side="top", fill="x", padx=10)
-            execute_button = ttk.Button(tabs[tab], text="Execute", command=lambda: send_button())
+            execute_button = ttk.Button(tabs[tab], text="Execute",
+                                        command=lambda: send_button(client_socket, command_entry, msg_list, command))
             execute_button.pack(side="top", fill="x", padx=10, pady=10)
 
         # Add elements to the "Power Management" tab
